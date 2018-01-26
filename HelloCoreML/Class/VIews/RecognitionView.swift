62dbdf84343af360217fb481499865204df88351
簡兆宏 <dabechen@DabeChens-MacBook-Pro.local>
2018-01-26 20:41:20 +0800

    First commit

---
+++ HelloCoreML/Class/VIews/RecognitionView.swift
@@ -0,0 +1,22 @@
+//
+//  RecognitionView.swift
+//  HelloCoreML
+//
+//  Created by DabeChen on 2018/1/18.
+//  Copyright © 2018年 DabeChen. All rights reserved.
+//
+
+import UIKit
+
+class RecognitionView: UIView {
+    
+    @IBOutlet weak var identifierLabel: UILabel!
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
